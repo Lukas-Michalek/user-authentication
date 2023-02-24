ff507f728d7bb3aa907dcf3d45f71bacc63a5f16
Luke <luke.michalek@gmail.com>
2023-02-24 21:00:02 +0000

    Add EJS Views files
    -Added EJS Viw files and basic templates for Login and Register route

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+
+// Middleware
+
+// This will tell our app to use ejs view engine to render all the files
+app.set('view engine','ejs');
+
+
+// Main Routes
+
+app.get('/', (request,response) => {
+    response.render("index");
+})
+
+app.get('/users/register', (request, response) => {
+    response.render("register")
+})
+
+app.get('/users/login', (request,response) => {
+    response.render("login");
+})
+
+app.get('/users/dashboard', (request,response) => {
+    response.render("dashboard", { user: 'Luke'});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening in ${PORT}`);
+})
